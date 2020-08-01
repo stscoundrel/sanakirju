22d97cac2d44ebf897d38451f6f434dfd4652070
stscoundrel <sampo@pixels.fi>
2020-08-01 11:03:38 +0300

    Scraper: add constants

---
+++ src/constants/scraper.js
@@ -0,0 +1,8 @@
+const DICTIONARY_URL = 'http://kaino.kotus.fi/cgi-bin/kks/karjala.cgi'
+const LETTERS = ['A', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Ä', 'Ö']
+
+module.exports = {
+  DICTIONARY_URL,
+  LETTERS,
+}
