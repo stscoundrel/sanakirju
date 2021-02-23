dafd1bb95434cd74669020a51872c61d90a66cc9
stscoundrel <sampo@pixels.fi>
2021-02-23 17:09:42 +0200

    Meanings: add test cases for getting meanings of RawEntry

--- tests/services/meanings.test.js
+++ tests/services/meanings.test.js
@@ -5,10 +5,23 @@ describe('Meanings service', () => {
   test('Detects if entry has multiple meanings', async () => {
     const entry = entryFixture;
 
-    const result1 = await meaningService.hasMultipleMeanings(entry);
-    const result2 = await meaningService.hasMultipleMeanings(entryFixtureMultipleMeanings);
+    const result1 = meaningService.hasMultipleMeanings(entry);
+    const result2 = meaningService.hasMultipleMeanings(entryFixtureMultipleMeanings);
 
     expect(result1).toBeFalsy();
     expect(result2).toBeTruthy();
+  });
+
+  test('Gets meaning from an entry', async () => {
+    const entry = entryFixture;
+
+    // Simple entry.
+    const result1 = meaningService.getMeaning(entry);
+
+    // Complex entry. Pass single definition, simulating word service.
+    const result2 = meaningService.getMeaning(entryFixtureMultipleMeanings.SenseGrp[0]);
+
+    expect(result1).toBe('pelätä.');
+    expect(result2).toBe('kikattaa.');
   });
 });
