46ee320685f236822b12080f67d6b9d1c710aaaa
stscoundrel <sampo@pixels.fi>
2021-02-24 10:15:53 +0200

    Tests: add 3rd raw entry fixture, add to relevant tests

--- tests/services/word-types.test.ts
+++ tests/services/word-types.test.ts
@@ -1,12 +1,14 @@
 import { getType } from '../../src/services/word-types';
-import { entryFixture, entryFixtureMultipleMeanings } from './fixtures/entry-fixtures.ts';
+import { entryFixture, entryFixtureMultipleMeanings, thirdEntry } from './fixtures/entry-fixtures.ts';
 
 describe('Word types service', () => {
   test('Gets word types from entries', async () => {
     const result1 = getType(entryFixture);
     const result2 = getType(entryFixtureMultipleMeanings);
+    const result3 = getType(thirdEntry);
 
     expect(result1).toEqual([]); // This fixture has no defined type.
     expect(result2).toEqual(['verb']);
+    expect(result3).toEqual(['verb']);
   });
 });
