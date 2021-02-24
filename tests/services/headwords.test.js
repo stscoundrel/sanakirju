a6810300535c7dc13f2c1f92f7d475143f65386c
stscoundrel <sampo@pixels.fi>
2021-02-24 15:30:55 +0200

    Tests: add headwords tests

---
+++ tests/services/headwords.test.js
@@ -0,0 +1,14 @@
+import { getHeadWord } from '../../src/services/headwords';
+import { entryFixture, entryFixtureMultipleMeanings, thirdEntry } from './fixtures/entry-fixtures.ts';
+
+describe('Headwords service', () => {
+  test('Gets headwords from entries', async () => {
+    const result1 = getHeadWord(entryFixture);
+    const result2 = getHeadWord(entryFixtureMultipleMeanings);
+    const result3 = getHeadWord(thirdEntry);
+
+    expect(result1).toEqual('keilakko');
+    expect(result2).toEqual('keketteä');
+    expect(result3).toEqual('önkähytteä');
+  });
+});
