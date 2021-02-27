2157c7822ce3781ee88049dbde4f5f3dc692af75
stscoundrel <sampo@pixels.fi>
2021-02-27 20:57:54 +0200

    Add Stryker for mutation testing
    Configure to only test unit tests. Add own jest config for running it.

---
+++ stryker.conf.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: 'yarn',
+  reporters: ['html', 'clear-text', 'progress'],
+  jest: {
+    projectType: 'custom',
+    configFile: 'jest.config.stryker.ts',
+  },
+  testRunner: 'jest',
+  coverageAnalysis: 'perTest',
+  tsconfigFile: 'tsconfig.json',
+};
