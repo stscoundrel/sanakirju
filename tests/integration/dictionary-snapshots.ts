a1a8127a4257acd30e579537ad6323e0005bd544
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-29 16:30:00 +0200

    Additional integration tests random sampling

--- tests/integration/dictionary-snapshots.ts
+++ tests/integration/dictionary-snapshots.ts
@@ -1,4 +1,4 @@
-import sanakirjuModule from '../../src';
+import sanakirjuModule, { getRawDictionary, getDictionary } from '../../src';
 
 const dictionarySnapshotTests = (sanakirju: typeof sanakirjuModule) : void => {
   describe('Dictionary content snapshot tests', () => {
@@ -8,10 +8,22 @@ const dictionarySnapshotTests = (sanakirju: typeof sanakirjuModule) : void => {
       expect(result.slice(0, 100)).toMatchSnapshot();
     });
 
+    test('RawEntry data is as expected, further entries, named export.', async () => {
+      const result = await getRawDictionary();
+
+      expect(result.slice(1900, 2000)).toMatchSnapshot();
+    });
+
     test('Formatted dictionary data is as expected.', async () => {
       const result = await sanakirju.getDictionary();
 
       expect(result.slice(0, 100)).toMatchSnapshot();
+    });
+
+    test('Formatted dictionary data is as expected, further entries, named export.', async () => {
+      const result = await getDictionary();
+
+      expect(result.slice(1900, 2000)).toMatchSnapshot();
     });
   });
 };
